25354da8bda94550bb1c68dd5fbfdf938febb3c8
- <->
2020-08-06 19:28:14 +0700

    ConnectDB-Model-Route

---
+++ routes/auth.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.router();
+
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+
+router.post("/register", async (req, res) => {});
+module.exports = router;
