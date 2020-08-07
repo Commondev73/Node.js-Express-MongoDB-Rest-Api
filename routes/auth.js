09a6237a5382ed0d1de4ae029c6c77ebe2e120a1
- <->
2020-08-07 08:13:45 +0700

    complete

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,34 @@
 const express = require("express");
-const router = express.router();
+const router = express.Router();
 
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
+const registerValidation = require("../validation/register");
 
-router.post("/register", async (req, res) => {});
+router.post("/register", async (req, res) => {
+  try {
+    const { name, username, password } = req.body;
+
+    const { error, value } = registerValidation.validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const usernameExist = await User.findOne({ username: username });
+    if(usernameExist) return res.status(400).send("username already exists")
+    
+    const salt = bcrypt.genSaltSync(10);
+    const hashPassword = bcrypt.hashSync(password, salt);
+
+    const user = new User({
+      name: name,
+      username: username,
+      password: hashPassword,
+    });
+
+    const saveUser = await user.save();
+    res.send(saveUser);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
 module.exports = router;
