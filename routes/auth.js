bfaab19db0514957e96a74bb88272bdb4b85a913
- <->
2020-08-08 07:24:03 +0700

    jwt

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
 const registerValidation = require("../validation/register");
+const loginValidation = require("../validation/login");
+
+const generateAccessToken = user => {
+  return  jwt.sign({_id:user._id , name: user.name},process.env.ACCSESS_TOKEN_SECRET,{ expiresIn:"60s" });
+}
+
+const generateRefreshToken = user => {
+  return  jwt.sign({_id:user._id , name: user.name},process.env.REFRESH_TOKEN_SECRET);
+}
 
 router.post("/register", async (req, res) => {
   try {
@@ -13,8 +23,8 @@ router.post("/register", async (req, res) => {
     if (error) return res.status(400).send(error.details[0].message);
 
     const usernameExist = await User.findOne({ username: username });
-    if(usernameExist) return res.status(400).send("username already exists")
-    
+    if (usernameExist) return res.status(400).send("username already exists");
+
     const salt = bcrypt.genSaltSync(10);
     const hashPassword = bcrypt.hashSync(password, salt);
 
@@ -26,6 +36,28 @@ router.post("/register", async (req, res) => {
 
     const saveUser = await user.save();
     res.send(saveUser);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const { error, value } = loginValidation.validate(req.body);
+    if (error) return res.status(400).json(error.details[0].message);
+
+    const user = await User.findOne({ username: username });
+    if (!user) return res.status(400).json("username is wrong");
+
+    const PwIsCorrect = bcrypt.compareSync(password, user.password);
+    if (!PwIsCorrect) return res.status(400).json("password is wrong");
+
+    const accessToken = generateAccessToken(user)
+    const refreshToken = generateRefreshToken(user)
+    res.json({accessToken: accessToken , refreshToken : refreshToken})
+
   } catch (error) {
     res.status(400).send(error);
   }
